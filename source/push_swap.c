a57e56c401efdfd6bee831c321ff40ba058ea265
Hamza Et-Tajani <het-taja@c3r8p6.1337.ma>
2024-05-13 12:08:58 +0100

    norminette for push_swap.c

--- source/push_swap.c
+++ source/push_swap.c
@@ -6,11 +6,34 @@
 /*   By: het-taja <het-taja@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 11:41:04 by het-taja          #+#    #+#             */
-/*   Updated: 2024/05/10 14:45:37 by het-taja         ###   ########.fr       */
+/*   Updated: 2024/05/13 12:08:26 by het-taja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	sort_after(t_stack **stack_a, t_stack **stack_b, char **str)
+{
+	int	i;
+
+	i = double_len(str);
+	if (i > 3)
+	{
+		if (!is_sorted(stack_a))
+			exit (1);
+		stack_b = malloc(sizeof(t_stack *));
+		(*stack_b) = NULL;
+		if (!stack_b)
+			exit (1);
+		if (i == 4)
+			sort_4(stack_a, stack_b);
+		else if (i == 5)
+			sort_5(stack_a, stack_b);
+		else
+			sort_more(stack_a, stack_b);
+		free(stack_b);
+	}
+}
 
 int	main(int ac, char **av)
 {
@@ -34,22 +57,7 @@ int	main(int ac, char **av)
 	set_final_mark(stack_a);
 	if (i <= 3)
 		sort_3(stack_a);
-	if (i > 3)
-	{
-		if (!is_sorted(stack_a))
-			return (0);
-		stack_b = malloc(sizeof(t_stack *));
-		(*stack_b) = NULL;
-		if (!stack_b)
-			return (0);
-		if (i == 4)
-			sort_4(stack_a, stack_b);
-		else if (i == 5)
-			sort_5(stack_a, stack_b);
-		else
-			sort_more(stack_a, stack_b);
-		free(stack_b);
-	}
+	sort_after(stack_a, stack_b, str);
 	free(nbr);
 	clear_str(str);
 	ft_lstclear(stack_a);
